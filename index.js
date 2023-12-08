3103b85a6d10e926ce4fd0865ab1fbaecfe03237
siomcgill15 <151243978+siomcgill15@users.noreply.github.com>
2023-12-08 16:50:38 +0000

    Updated html & JS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function refreshWeather(response){
- let temperatureElement = document.querySelector("#temperature");
- let temperature = response.data.temperature.current;
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-  let date = new Date (response.data.time * 1000)
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
@@ -14,49 +14,39 @@ function refreshWeather(response){
   humidityElement.innterHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-
+}
 function formatDate(date) {
-   
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday,"
-        "Thursday,"
-        "Friday",
-        "Saturday",
-    
-    ];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-     let day = days[date.getDay()];
+  let day = days[date.getDay()];
 
-     if (minutes <10) {
-minutes = `0${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-     }
-
-
-return `${day} ${hours}:${minutes}`;
-
+  return `${day} ${hours}:${minutes}`;
 }
- function searchCity(city) {
-let apiKey = "b2a5adcct0"
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=$(city)"
-axios.get(api.Url).then(refreshWeather);
-
+function searchCity(city) {
+  let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handSearchSubmit(event) {
-event.preventDefault();
-let searchInput = document.querySelector("#search-form-input");
-
-searchCity(searchInput.value);
-
+  event.preventDefault();
+  let searchInput = document.querySelector(".search-form-input");
+  searchCity(searchInput.value);
 }
-
-let searchFormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handSearchSubmit);
 
 searchCity("Paris");
-
- 
